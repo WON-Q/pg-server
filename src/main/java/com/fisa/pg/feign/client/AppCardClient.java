0e093b197cec2e141d9a00ab51a43ca0056c34a2
namsh1125 <namsh1125@naver.com>
2025-05-08 17:29:54 +0900

    Feat: 앱카드 결제 인증 요청 API 구현
    Co-authored-by: 김새봄 <sae051@hufs.ac.kr>
    Co-authored-by: 남승현 <namsh1125@naver.com>

---
+++ src/main/java/com/fisa/pg/feign/client/AppCardClient.java
@@ -0,0 +1,28 @@
+package com.fisa.pg.feign.client;
+
+import com.fisa.pg.feign.dto.appcard.request.AppCardAuthRequestDto;
+import com.fisa.pg.feign.dto.appcard.response.AppCardAuthResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "appCardClient", url = "${app.card.token")
+public interface AppCardClient {
+
+    /**
+     * 앱카드 결제 인증 요청 API
+     * <ul>
+     *     <li>결제 인증 요청 (결제 흐름 중 <b>16번째 단계</b>)</li>
+     *     <li>딥링크 생성 및 반환 (결제 흐름 중 <b>19번째 단계</b>)</li>
+     * </ul>
+     * 자세한 내용은 프로젝트 내 {@code docs/payment-flow.md} 문서를 참조해 주세요.
+     *
+     * @param request 결제 인증 요청 정보
+     * @return 앱카드 인증 응답 (딥링크 포함)
+     */
+    @PostMapping("/authentication")
+    AppCardAuthResponseDto requestAuth(
+            @RequestBody AppCardAuthRequestDto request
+    );
+
+}
