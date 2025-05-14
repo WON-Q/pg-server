1f6cb8e80f42c77e842e172cb95be58386aa0583
saebomnewspring <sae051@hufs.ac.kr>
2025-05-14 11:24:55 +0900

    Feat: AppCardClient 구현

--- src/main/java/com/fisa/pg/feign/client/AppCardClient.java
+++ src/main/java/com/fisa/pg/feign/client/AppCardClient.java
@@ -4,6 +4,7 @@ import com.fisa.pg.config.feign.AppCardClientConfig;
 import com.fisa.pg.feign.dto.appcard.request.AppCardAuthRequestDto;
 import com.fisa.pg.feign.dto.appcard.response.AppCardAuthResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,18 +12,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface AppCardClient {
 
     /**
-     * 앱카드 결제 인증 요청 API
-     * <ul>
-     *     <li>결제 인증 요청 (결제 흐름 중 <b>16번째 단계</b>)</li>
-     *     <li>딥링크 생성 및 반환 (결제 흐름 중 <b>19번째 단계</b>)</li>
-     * </ul>
+     * 앱카드 인증 요청 API
+     * <br/>
+     * 이 API는 결제 흐름 중 <b>16번째 단계</b>에서 사용됩니다.
+     * PG사에서 앱카드 서버로 인증을 요청합니다.
      * 자세한 내용은 프로젝트 내 {@code docs/payment-flow.md} 문서를 참조해 주세요.
      *
-     * @param request 결제 인증 요청 정보
-     * @return 앱카드 인증 응답 (딥링크 포함)
+     * @param request 앱카드 인증 요청 정보
+     * @return 앱카드 인증 응답
      */
-    @PostMapping("/authentication")
-    AppCardAuthResponseDto requestAuth(
+    @PostMapping("/auth")
+    ResponseEntity<AppCardAuthResponseDto> requestAppCardAuth(
             @RequestBody AppCardAuthRequestDto request
     );
 
