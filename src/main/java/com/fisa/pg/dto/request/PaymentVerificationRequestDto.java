ea980bd04a3be5c76cac9d75a161c48c3ec386dd
namsh1125 <namsh1125@naver.com>
2025-05-14 21:32:50 +0900

    Feat: 결제 검증 API의 요청/응답 DTO 구현
    Co-authored-by: 김새봄 <sae051@hufs.ac.kr>
    Co-authored-by: 남승현 <namsh1125@naver.com>

---
+++ src/main/java/com/fisa/pg/dto/request/PaymentVerificationRequestDto.java
@@ -0,0 +1,22 @@
+package com.fisa.pg.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 클라이언트(가맹점)으로부터 PG 서버로 전달되는 결제 검증 요청 DTO
+ * <br />
+ * 이 DTO는 결제 흐름 중 <b>44번째 단계</b>에서 사용됩니다.
+ * 자세한 내용은 프로젝트 내 {@code docs/payment-flow.md} 문서를 참조해 주세요.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentVerificationRequestDto {
+
+    // Todo: 현재 원큐오더 웹하고 상의가 되지 않아 해당 부분에 대해 논의를 진행하고 작성 예정
+
+}
---
+++ src/main/java/com/fisa/pg/dto/response/PaymentVerificationResponseDto.java
@@ -0,0 +1,13 @@
+package com.fisa.pg.dto.response;
+
+/**
+ * 클라이언트(가맹점)으로부터 PG 서버로 전달되는 결제 검증 응답 DTO
+ * <br />
+ * 이 DTO는 결제 흐름 중 <b>46번째 단계</b>에서 사용됩니다.
+ * 자세한 내용은 프로젝트 내 {@code docs/payment-flow.md} 문서를 참조해 주세요.
+ */
+public class PaymentVerificationResponseDto {
+
+    // Todo: 현재 원큐오더 웹하고 상의가 되지 않아 해당 부분에 대해 논의를 진행하고 작성 예정
+
+}
