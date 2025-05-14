77036b0482980718df57a34695b7135169c32128
namsh1125 <namsh1125@naver.com>
2025-05-15 00:52:37 +0900

    Feat: Access Key 기반으로 API 키 조회하는 메서드 구현
    Co-authored-by: 김새봄 <sae051@hufs.ac.kr>
    Co-authored-by: 남승현 <namsh1125@naver.com>

--- src/main/java/com/fisa/pg/repository/ApiKeyRepository.java
+++ src/main/java/com/fisa/pg/repository/ApiKeyRepository.java
@@ -3,6 +3,15 @@ package com.fisa.pg.repository;
 import com.fisa.pg.entity.auth.ApiKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
 
+    /**
+     * Access Key 기반으로 API 키 조회하는 메서드
+     *
+     * @param accessKey 조회할 액세스 키
+     * @return 액세스 키와 일치하는 API 키
+     */
+    Optional<ApiKey> findByAccessKey(String accessKey);
 }
