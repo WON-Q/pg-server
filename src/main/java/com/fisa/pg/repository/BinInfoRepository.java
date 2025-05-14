1dd69446666b8e480a4d80a194713b38ecdc89e8
namsh1125 <namsh1125@naver.com>
2025-05-14 22:11:14 +0900

    Feat: Bin 정보 관리용 JpaRepository 구현
    Co-authored-by: 김새봄 <sae051@hufs.ac.kr>
    Co-authored-by: 남승현 <namsh1125@naver.com>

---
+++ src/main/java/com/fisa/pg/repository/BinInfoRepository.java
@@ -0,0 +1,8 @@
+package com.fisa.pg.repository;
+
+import com.fisa.pg.entity.card.BinInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BinInfoRepository extends JpaRepository<BinInfo, String> {
+
+}
