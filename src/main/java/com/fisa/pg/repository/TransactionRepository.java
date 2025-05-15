a182c1bd26051aba4157ec15ac4d641a838e43f1
namsh1125 <namsh1125@naver.com>
2025-05-15 10:16:48 +0900

    Feat: 특정 결제(Payment)와 트랜잭션 상태로 트랜잭션을 조회하는 메서드 구현
    Co-authored-by: 김새봄 <sae051@hufs.ac.kr>
    Co-authored-by: 남승현 <namsh1125@naver.com>

--- src/main/java/com/fisa/pg/repository/TransactionRepository.java
+++ src/main/java/com/fisa/pg/repository/TransactionRepository.java
@@ -1,8 +1,21 @@
 package com.fisa.pg.repository;
 
+import com.fisa.pg.entity.payment.Payment;
 import com.fisa.pg.entity.transaction.Transaction;
+import com.fisa.pg.entity.transaction.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
+import java.util.Optional;
+import java.util.UUID;
 
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+    /**
+     * 특정 결제(Payment)와 트랜잭션 상태로 트랜잭션을 조회하는 메서드
+     *
+     * @param payment 조회할 결제 객체
+     * @param status  조회할 트랜잭션 상태
+     * @return 조건에 맞는 트랜잭션 (없을 경우 빈 Optional)
+     */
+    Optional<Transaction> findByPaymentAndTransactionStatus(Payment payment, TransactionStatus status);
 }
