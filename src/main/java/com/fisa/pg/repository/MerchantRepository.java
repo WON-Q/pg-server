f9aa94524818a39008ddb3fed4b011d9e2fa7fed
saebomnewspring <sae051@hufs.ac.kr>
2025-05-19 20:48:39 +0900

    Feat: 웹훅 URL이 등록되고 활성화된 가맹점 목록 조회하는 메서드 구현
    Co-authored-by: 김새봄 <sae051@hufs.ac.kr>
    Co-authored-by: 남승현 <namsh1125@naver.com>

--- src/main/java/com/fisa/pg/repository/MerchantRepository.java
+++ src/main/java/com/fisa/pg/repository/MerchantRepository.java
@@ -1,6 +1,8 @@
 package com.fisa.pg.repository;
 
 import com.fisa.pg.entity.user.Merchant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -14,4 +16,10 @@ public interface MerchantRepository extends JpaRepository<Merchant, Long> {
      * @return 가맹점 정보
      */
     Optional<Merchant> findByEmail(String email);
+
+    /**
+     * 웹훅 URL이 등록되고 활성화된 가맹점 목록 조회하는 메서드
+     */
+    Page<Merchant> findByWebhookUrlIsNotNullAndIsWebhookEnabledTrue(Pageable pageable);
+
 }
