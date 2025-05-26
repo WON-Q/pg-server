97614e24b96ccb922f4977a2c6a955552e60d436
taegung <yseongmin12@gmail.com>
2025-05-26 09:35:08 +0900

    refactor: notblank 수정

--- src/main/java/com/fisa/pg/dto/request/CreateApiKeyRequestDto.java
+++ src/main/java/com/fisa/pg/dto/request/CreateApiKeyRequestDto.java
@@ -18,7 +18,7 @@ public class CreateApiKeyRequestDto {
     @NotBlank(message = "이름은 필수입니다.")
     private String name;
 
-    @NotBlank(message = "액세스 날짜는 필수입니다.")
+
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime expiresAt;
 
