e13dc5327e64e6405ac3688fde1e387adf4bc9c6
yxhwxn <officeyh9819@gmail.com>
2025-05-27 19:56:29 +0900

    Fix: HostKeyVerifier 인터페이스 필수 메서드("findExistingAlgorithms()")  추가

--- src/main/java/com/fisa/pg/batch/utils/SftpUploader.java
+++ src/main/java/com/fisa/pg/batch/utils/SftpUploader.java
@@ -2,11 +2,18 @@ package com.fisa.pg.batch.utils;
 
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.transport.verification.HostKeyVerifier;
+import net.schmizz.sshj.xfer.LocalSourceFile;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.PublicKey;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class SftpUploader {
@@ -32,14 +39,48 @@ public class SftpUploader {
         }
 
         try (SSHClient ssh = new SSHClient()) {
-            ssh.addHostKeyVerifier((hostname, port, key) -> true); // TODO: 실 운영 시엔 known_hosts 적용
+
+            ssh.addHostKeyVerifier(new HostKeyVerifier() {
+                @Override
+                public boolean verify(String hostname, int port, PublicKey key) {
+                    return true; // 실 서비스에서는 known_hosts 기반 검증을 해야 함
+                }
+
+                @Override
+                public List<String> findExistingAlgorithms(String hostname, int port) {
+                    // 안전하게 비워진 리스트 반환
+                    return Collections.emptyList();
+                }
+            });
+
             ssh.connect(sftpHost, sftpPort);
             ssh.authPassword(sftpUsername, sftpPassword);
 
-            try (SFTPClient sftp = ssh.newSFTPClient();
-                 FileInputStream fis = new FileInputStream(file)) {
+            try (SFTPClient sftp = ssh.newSFTPClient()) {
 
-                sftp.put(fis, sftpRemoteDir + "/" + file.getName());
+                LocalSourceFile sourceFile = new LocalSourceFile() {
+                    @Override
+                    public String getName() {
+                        return file.getName();
+                    }
+
+                    @Override
+                    public long getLength() {
+                        return file.length();
+                    }
+
+                    @Override
+                    public InputStream getInputStream() throws IOException {
+                        return new FileInputStream(file); // 이 시점에 스트림 열기
+                    }
+
+                    @Override
+                    public boolean isFile() {
+                        return true;
+                    }
+                };
+
+                sftp.put(sourceFile, sftpRemoteDir + "/");
             }
 
         } catch (IOException e) {
